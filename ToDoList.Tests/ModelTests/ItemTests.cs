f02aaf40e7fcc171cc80115865bef871f61c2da7
Svea Wade <svealinneawade@gmail.com>
2020-12-09 09:53:39 -0800

    Passed two tests
    Co-Authored-by:Amanda Otero <mandystar018@gmail.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests
+  {
+    [TestMethod]
+    public void ItemConstructor_CreatedInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+        [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+  }
+}
